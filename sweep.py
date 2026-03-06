d24531cb830aece2bf42ac47d67f212bad100a3d
Lương Tiến Khanh <128344602+khanzil@users.noreply.github.com>
2026-03-06 16:23:14 +0700

    .

--- sweep.py
+++ sweep.py
@@ -1,4 +1,3 @@
-from tqdm import tqdm
 import argparse
 import numpy as np
 import itertools
@@ -11,7 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Sweep for hyperparameter search",add_help=True)
     parser.add_argument('-c', '--config_file', help='Specify config file', metavar='FILE')
     parser.add_argument('--n_trials', type=int, default=3, help='Number of trials for each algo')
-    parser.add_argument('--n_search', type=int, default=20, help='Number of hyperparameter searchs')
+    parser.add_argument('--n_searchs', type=int, default=20, help='Number of hyperparameter searchs')
     parser.add_argument('--seed', type=int, default=0, help='Seed for sweeping')
     parser.add_argument('--n_test_doms', type=int, default=1, help='Number of test domains')
     args = parser.parse_args()
@@ -29,21 +28,23 @@ if __name__ == '__main__':
     
     for trial_seed in range(args.n_trials):
         # only support single test domain for now
-        for search in range(args.n_search):
+        for search in range(args.n_searchs):
             
             new_cfg = get_random_search_configs(cfg, train_id)
             cfg_yaml_list.append(f'./configs/sweep/config_{train_id}.yaml')
-
+            
+            print(type(new_cfg['featurizer']))
             # create config_{i}.yaml for each cfg
             with open(cfg_yaml_list[-1], 'w') as f:
                 yaml.dump(new_cfg, f)
+
             train_id += 1
             
 
     # run subprocesses for each congis_{i}.yaml
-    for seed, cfg_yaml in enumerate(cfg_yaml_list):
-        print(f'Starting {cfg_yaml}')
-        subprocess.call(f'python train.py -c {cfg_yaml} train --num_workers=4 --seed={seed}', shell=True)
+    # for seed, cfg_yaml in enumerate(cfg_yaml_list):
+    #     print(f'Starting {cfg_yaml}')
+    #     subprocess.call(f'python train.py -c {cfg_yaml} train --num_workers=4 --seed={seed}', shell=True)
 
 
 
--- utils/sweep_utils.py
+++ utils/sweep_utils.py
@@ -15,13 +15,12 @@ def get_random_search_configs(cfg, train_id):
     # featurizer-based cfg
     if new_cfg['featurizer'] == 'MLP':
         new_cfg['mlp_num_hidden'] = 2**np.random.randint(5,8)
-        new_cfg['mlp_dropout'] = np.random.choice([0.0, 0.1, 0.5])
+        new_cfg['mlp_dropout'] = float(np.random.choice([0.0, 0.1, 0.5]))
         new_cfg['mlp_width'] = 2**np.random.randint(6,10)
         new_cfg['mlp_depth'] = np.random.randint(3,5)
     
     elif new_cfg['featurizer'] == 'ResNet':
-        new_cfg['resnet_dropout'] = np.random.choice([0.0, 0.1, 0.5])
-
+        new_cfg['resnet_dropout'] = float(np.random.choice([0.0, 0.1, 0.5]))
 
     # algo-based cfg
     if new_cfg['algorithm'] == 'DANN':
