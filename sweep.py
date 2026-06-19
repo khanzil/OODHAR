775dd0078ecab593967f992b36155e8fb79115e4
Lương Tiến Khanh <128344602+khanzil@users.noreply.github.com>
2026-06-19 18:51:02 +0700

    Update sweep.py

--- sweep.py
+++ sweep.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Sweep for hyperparameter search",add_help=True)
     parser.add_argument('-c', '--config_file', help='Specify config file', metavar='FILE')
     parser.add_argument('--n_trials', type=int, default=3, help='Number of trials for each algo, affect how data is divided')
-    parser.add_argument('--trial_start', type=int, default=0, help='To continue search if needed')
-    parser.add_argument('--n_searchs', type=int, default=20, help='Number of hyperparameter searchs')
+    parser.add_argument('--seed_start', type=int, default=0, help='To continue search if needed')
+    parser.add_argument('--n_searchs', type=int, default=4, help='Number of hyperparameter searchs')
     parser.add_argument('--trial_seed', type=int, default=0, help='Seed for sweeping, affect how hyperparameter is generated')
     parser.add_argument('--n_test_doms', type=int, default=1, help='Number of test domains')
     parser.add_argument('--algo', type=str)
@@ -29,11 +29,11 @@ if __name__ == '__main__':
     cfg_yaml_list = []
     train_id = 0
     
-    for seed in range(args.trial_start, args.n_trials):
+    for seed in range(args.n_trials):
         # only support single test domain for now, this seed controls RNG for dataset divison
         for search in range(args.n_searchs):
             new_cfg = get_random_search_configs(cfg, seed, search, args.algo, args.featurizer)
-            cfg_yaml_list.append(f"./configs/sweep/config_{new_cfg['train_id']}.yaml")
+            cfg_yaml_list.append((f"./configs/sweep/config_{new_cfg['train_id']}.yaml",seed,search))
             # create config_{i}.yaml for each cfg
             with open(cfg_yaml_list[-1], 'w') as f:
                 yaml.dump(new_cfg, f)
@@ -42,7 +42,9 @@ if __name__ == '__main__':
             
 
     # # run subprocesses for each congis_{i}.yaml
-    for seed, cfg_yaml in enumerate(cfg_yaml_list):
+    for i, (cfg_yaml,seed,search) in enumerate(cfg_yaml_list):
+        if search < args.start_trials:
+            continue
         print(f'Starting {cfg_yaml}')
         subprocess.call(f'python train.py -c {cfg_yaml} train --num_workers=4 --seed={seed}', shell=True)
 
