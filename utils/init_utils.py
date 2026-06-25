f2100b1fe5bc2efc5b0294656d308d5dd82b9f27
Lương Tiến Khanh <128344602+khanzil@users.noreply.github.com>
2026-06-25 11:35:54 +0700

    Update init_utils.py

--- utils/init_utils.py
+++ utils/init_utils.py
@@ -31,7 +31,9 @@ def get_dataloader(cfgs, args):
     dom_list = [fold for fold in os.listdir(dataset_dir) if os.path.isdir(os.path.join(dataset_dir,fold))]
     loaders = []
     dataset_num_workers = -1
-
+    generator = torch.Generator()
+    generator.manual_seed(args.seed)
+    
     if cfgs['test_dom'] == 'None':
         # This code is using train-valication domain split and sweep through all test domain combination
         for i_test_dom, test_dom in enumerate(dom_list):
@@ -49,7 +51,7 @@ def get_dataloader(cfgs, args):
                 else:
                     train_dataset, val_dataset = random_split(dataset, 
                                                               [int(cfgs['train_split']*len(dataset)), int((1-cfgs['train_split'])*len(dataset))],
-                                                              generator=torch.Generator.manual_seed(args.seed))
+                                                              generator=generator)
 
                     train_weights = make_weights_for_balanced_classes(train_dataset)
 
@@ -92,7 +94,7 @@ def get_dataloader(cfgs, args):
                                                                     [int(cfgs['train_split']*len(dataset)), 
                                                                      int((1-cfgs['train_split']-cfgs['test_split'])*len(dataset)),
                                                                      int(cfgs['test_split']*len(dataset))],
-                                                                    generator=torch.Generator.manual_seed(args.seed))
+                                                                    generator=generator)
 
             train_weights = make_weights_for_balanced_classes(train_dataset)
 
@@ -138,7 +140,7 @@ def get_dataloader(cfgs, args):
             else:
                 train_dataset, val_dataset = random_split(dataset, 
                                                             [int(cfgs['train_split']*len(dataset)), int((1-cfgs['train_split'])*len(dataset))],
-                                                            generator=torch.Generator.manual_seed(args.seed))
+                                                            generator=generator)
 
                 train_weights = make_weights_for_balanced_classes(train_dataset)
 
