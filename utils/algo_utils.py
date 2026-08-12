03a1d7305655164250dec30eea4ff40dafac57c6
Lương Tiến Khanh <128344602+khanzil@users.noreply.github.com>
2026-08-12 10:56:54 +0700

    Update algo_utils.py

--- utils/algo_utils.py
+++ utils/algo_utils.py
@@ -1000,7 +1000,7 @@ class CFSM(Algorithm):
 
         self.ClassPrototype = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(self.classifier.weight.shape[1],self.featurizer.n_outputs), # classifier.weight have shape (n_class, n_hidden)
+            nn.Linear(self.classifier[-1].weight.shape[1],self.featurizer.n_outputs), # classifier.weight have shape (n_class, n_hidden)
             nn.ReLU(),
             nn.Linear(self.featurizer.n_outputs,self.featurizer.n_outputs),
         )
