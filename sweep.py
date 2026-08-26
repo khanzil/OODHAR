cdc80dc701081c40e9f71b80287d63979e975aa7
Lương Tiến Khanh <128344602+khanzil@users.noreply.github.com>
2026-08-26 10:39:44 +0700

    Update sweep.py

--- sweep.py
+++ sweep.py
@@ -27,16 +27,17 @@ if __name__ == '__main__':
     for seed in range(args.trial_start, args.n_trials):
         # only support single test domain for now, this seed controls RNG for dataset divison
         for search in range(args.search_start,args.n_searchs+1):
-            cfg_yaml_list.append((f"./configs/sweep/config_seed{seed}_search{search}_{args.algo}_{args.featurizer}.yaml",seed,search))
+            train_cfg_dir = f"./configs/sweep/config_seed{seed}_search{search}_{args.algo}_{args.featurizer}.yaml"
+            # cfg_yaml_list.append((f"./configs/sweep/config_seed{seed}_search{search}_{args.algo}_{args.featurizer}.yaml",seed,search))
             # create config_{i}.yaml for each cfg
             cfgs['train_id'] = f"seed{seed}_search{search}_{args.algo}_{args.featurizer}"
             cfgs['algorithm'] = args.algo
             cfgs['featurizer'] = args.featurizer
-            with open(cfg_yaml_list[-1][0], 'w') as f:
+            with open(train_cfg_dir, 'w') as f:
                 yaml.dump(cfgs, f)
 
             print(f"Starting {cfgs['train_id']}")
-            subprocess.call(f"python train.py -c {cfgs} train --num_workers={args.num_workers} --seed={seed} --search={search}", shell=True)
+            subprocess.call(f"python train.py -c {train_cfg_dir} train --num_workers={args.num_workers} --seed={seed} --search={search}", shell=True)
             
 
     # # run subprocesses for each congis_{i}.yaml
